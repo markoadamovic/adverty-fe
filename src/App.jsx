d3bb07cc80e34b3dfc981c3e5a4a88e4d1b207c5
sv_ionity <stefan.veselinovic@partner.ionity.eu>
2025-10-31 12:53:28 +0100

    feat: Remove dahsboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./components/Login"
-import Devices from "./components/device/Devices"
-import DashboardLayout from "./components/DashboardLayout"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Devices from "./components/device/Devices";
 
 // Optional: other dashboard pages
-import HomeDashboard from "./components/dashboard/HomeDashboard.jsx"
-import Locations from "./components/Locations"
-import Campaigns from "./components/campaign/Campaigns"
-import CampaignDetail from "./components/campaign/CampaignDetail"
-import Register from "./components/auth/RegisterModal.jsx"
+import Locations from "./components/Locations";
+import Campaigns from "./components/campaign/Campaigns";
+import CampaignDetail from "./components/campaign/CampaignDetail";
+import Register from "./components/auth/RegisterModal.jsx";
 
 function App() {
   return (
@@ -19,19 +22,21 @@ function App() {
         <Route path="/register" element={<Register />} />
 
         {/* Dashboard routes with sidebar */}
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="home" element={<HomeDashboard />} />
+        <Route>
           <Route path="devices" element={<Devices />} />
           <Route path="locations" element={<Locations />} />
           <Route path="campaigns" element={<Campaigns />} />
-          <Route path="/dashboard/campaigns/:campaignId" element={<CampaignDetail />} />
+          <Route
+            path="/dashboard/campaigns/:campaignId"
+            element={<CampaignDetail />}
+          />
         </Route>
 
         {/* Redirect unknown routes */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
